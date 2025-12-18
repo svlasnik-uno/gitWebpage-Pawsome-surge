f8978ca3589eb98c0f94b3a47fd3868148eb2a09
svlasnik-uno <svlasnik@unomaha.edu>
2025-12-18 06:33:53 -0600

    First page

---
+++ README.md
@@ -0,0 +1,13 @@
+# pawsome arts and crafts
+Simple, HTML only webpage
+To publish to surge: In a Terminal/PowerShell window, enter surge press 
+enter for the project
+CNAME file defines the domain - current: PawsomeArtsAndCrafts.surge.sh
+Help: https://surge.sh/help/getting-started-with-surge
+Current page is defined in the CNAME file in this repository
+
+If the page fails to update on deployment - try 
+surge teardown PawsomeArtsAndCrafts
+.surge.sh
+
+then redeploy.
---
+++ css/scripts.js
@@ -0,0 +1,24 @@
+let slidePosition = 0;
+//SlideShow();
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow() {
+  let i;
+  let slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 3000); // Change image every 2 seconds
+}
