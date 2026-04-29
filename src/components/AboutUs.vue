dc3d5db3f62980f92ea2260ffeaa1611acb8f586
svlasnik-uno <svlasnik@unomaha.edu>
2026-04-29 14:39:43 -0500

    Update AboutUs.vue

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -29,9 +29,8 @@
           </p>
         </div>
         <div class="mt-3">
-          <h5>Our flowers are crafted from Sola Wood</h5>
+          <h5>What are Sola Wood Flowers? </h5>
           <p>
-             What are Sola Wood Flowers? 
              <br>Sola wood flowers are handcrafted from the wood of the <em>Aeschynomene aspera</em> plant.
             <br>We soften and hand-dye raw sola wood flowers for our custom arrangements.
           </p>
